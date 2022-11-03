220a4ac4330981d0723e5d24702f21a099773b8c
sandeepkaurs <sandeep.singh@telus.com>
2022-11-02 19:55:08 -0700

    Initial commit with all my work so far

---
+++ javascripting/if-statement.js
@@ -0,0 +1,2 @@
+let fruit = 'orange'
+if (fruit.length > 5) {console.log('The fruit name has more than five characters.')} else {('The fruit name has five characters or less.')}
---
+++ javascripting/looping-through-arrays.js
@@ -0,0 +1,7 @@
+const pets = ['cat', 'dog', 'rat']
+let i = 0
+for (let i = 0; i < pets.length; i++) {
+  pets[i] = pets[i] + 's';
+  
+}
+console.log(pets)
